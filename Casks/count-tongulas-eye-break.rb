7a802ce2bd9904111fc25ae95e5749be1d20c89e
alex tong <tongsalex@gmail.com>
2026-03-05 16:49:22 -0500

    Update cask to v0.10.0 (Sparkle auto-updates)

--- Casks/count-tongulas-eye-break.rb
+++ Casks/count-tongulas-eye-break.rb
@@ -1,6 +1,6 @@
 cask "count-tongulas-eye-break" do
-  version "0.9.1"
-  sha256 "9f812d7f73a279ecb00c2a4216ad41b93d5d034df7ae52c6825b8ccb3899ac7a"
+  version "0.10.0"
+  sha256 "bdd1cb990ca04b2236253affc98bcc8ea2d3000c1efbc7346e42dad9005859df"
 
   url "https://github.com/alextongme/count-tongulas-eye-break/releases/download/v#{version}/CountTongulasEyeBreak-#{version}.zip"
   name "Count Tongula's Eye Break"
